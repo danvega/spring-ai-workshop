08b7246e4856a551e566b9ac2317f51a59d7c6af
danvega <danvega@gmail.com>
2025-03-04 10:05:20 -0500

    Adding AsyncController

---
+++ src/main/java/dev/danvega/workshop/async/AsyncChatController.java
@@ -0,0 +1,25 @@
+package dev.danvega.workshop.async;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+public class AsyncChatController {
+
+    private final ChatClient chatClient;
+
+    public AsyncChatController(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
+
+    @GetMapping("/stream")
+    public Flux<String> stream() {
+        return chatClient.prompt()
+                .user("I am visiting Atlanta, GA can you give me 10 places I must visit")
+                .stream()
+                .content();
+    }
+
+}
