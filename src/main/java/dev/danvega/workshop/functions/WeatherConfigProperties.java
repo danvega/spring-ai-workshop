14f810f9c3f6bb88999993d05e681a3ad4ecce50
danvega <danvega@gmail.com>
2025-03-04 10:05:07 -0500

    Removing older functions example

--- src/main/java/dev/danvega/workshop/functions/WeatherConfigProperties.java
+++
@@ -1,8 +0,0 @@
-package dev.danvega.workshop.functions;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(value = "weather")
-public record WeatherConfigProperties(String apiKey, String apiUrl) {
-
-}
