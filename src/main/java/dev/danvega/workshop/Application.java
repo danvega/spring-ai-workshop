4efc3ff9f6fdd25719b6afa1df786a1e8cf1ced0
danvega <danvega@gmail.com>
2025-03-04 11:39:35 -0500

    No need for exceptions

--- src/main/java/dev/danvega/workshop/Application.java
+++ src/main/java/dev/danvega/workshop/Application.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) throws IOException, InterruptedException {
+	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
