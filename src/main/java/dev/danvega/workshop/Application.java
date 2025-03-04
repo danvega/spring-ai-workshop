cf64e764c12e1876accae910ba969d5f9a75a9a6
danvega <danvega@gmail.com>
2025-03-04 10:09:43 -0500

    Removing weather configuration used for old function calls

--- src/main/java/dev/danvega/workshop/Application.java
+++ src/main/java/dev/danvega/workshop/Application.java
@@ -1,13 +1,10 @@
 package dev.danvega.workshop;
 
-import dev.danvega.workshop.functions.WeatherConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 import java.io.IOException;
 
-@EnableConfigurationProperties(WeatherConfigProperties.class)
 @SpringBootApplication
 public class Application {
 
